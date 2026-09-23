a0f063922aa97dc79c92622b7a31686b057df78f
acampion <campionauguste@gmail.com>
2026-09-23 16:48:46 +0200

    added highscores assets

--- font.py
+++ font.py
@@ -1,7 +1,7 @@
 import pygame
 
 _ = pygame.init()
-screen = pygame.display.set_mode((1200, 720))
+screen = pygame.display.set_mode((1900, 1200))
 running = True
 while running:
     for event in pygame.event.get():
@@ -10,11 +10,11 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-    title = pygame.font.Font("pacman_font.ttf", 150)
-    play = title.render("Play", True, (255, 255, 0))
-    play_hovered = title.render("Play", True, (255, 155, 0))
+    title = pygame.font.Font("pacman_font.ttf", 100)
+    play = title.render("HIGHSCORES", True, (255, 255, 0))
+    # play_hovered = title.render("P", True, (255, 155, 0))
     _ = screen.blit(play, (200, 200))
-    _ = screen.blit(play_hovered, (200, 300))
-    pygame.image.save(play, "assets/play.png")
-    pygame.image.save(play_hovered, "assets/play_hovered.png")
+    # _ = screen.blit(play_hovered, (200, 300))
+    pygame.image.save(play, "assets/highscores.png")
+    # pygame.image.save(play_hovered, "assets/play_hovered.png")
     pygame.display.flip()
